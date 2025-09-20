f213bd691a503d05fcb3f728964c26e4122a754b
ViniciusCamargoL <viniciuscl843@gmail.com>
2025-09-20 14:26:17 -0300

    Add files via upload

---
+++ Grupo Limpeza/js/script.js
@@ -0,0 +1,18 @@
+document.getElementById('gruposSelecionado').addEventListener('click', function() {
+    const selecao1 = document.getElementById('selecao1');
+    const selecao2 = document.getElementById('selecao2');
+    
+    const grupo1 = selecao1.options[selecao1.selectedIndex].text;
+    const grupo2 = selecao2.options[selecao2.selectedIndex].text;
+    
+    if (grupo1 === grupo2) {
+        alert("[ERRO] O mesmo grupo foi selecionado, verefique os grupos selecionados novamente.");
+    } else {
+        document.getElementById('content').innerHTML = "";
+        alert(`Os grupos selecionados foram: ${grupo1} e ${grupo2}`);
+        const imagem = document.createElement = ("img");
+        imagem.src = "../img/fundo.png";
+    
+        document.getElementById("content").appendChild(novaImagem);
+    }
+});
